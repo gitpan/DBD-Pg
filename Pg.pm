9b943a69182980308a67843814b940b4ce5feb4b
Greg Sabino Mullane <greg@turnstep.com>
2008-09-01 01:40:52 +0000

    Import of TURNSTEP/DBD-Pg-2.10.2 from CPAN.
    gitpan-cpan-distribution: DBD-Pg
    gitpan-cpan-version:      2.10.2
    gitpan-cpan-path:         TURNSTEP/DBD-Pg-2.10.2.tar.gz
    gitpan-cpan-author:       TURNSTEP
    gitpan-cpan-maturity:     released

--- Pg.pm
+++ Pg.pm
@@ -1,5 +1,5 @@
 #  -*-cperl-*-
-#  $Id: Pg.pm 11692 2008-08-31 15:38:20Z turnstep $
+#  $Id: Pg.pm 11704 2008-09-01 01:39:35Z turnstep $
 #
 #  Copyright (c) 2002-2008 Greg Sabino Mullane and others: see the Changes file
 #  Portions Copyright (c) 2002 Jeffrey W. Baker
@@ -17,7 +17,7 @@ use 5.006001;
 {
 	package DBD::Pg;
 
-	use version; our $VERSION = qv('2.10.1');
+	use version; our $VERSION = qv('2.10.2');
 
 	use DBI ();
 	use DynaLoader ();
@@ -1703,7 +1703,7 @@ DBD::Pg - PostgreSQL database driver for the DBI module
 
 =head1 VERSION
 
-This documents version 2.10.1 of the DBD::Pg module
+This documents version 2.10.2 of the DBD::Pg module
 
 =head1 DESCRIPTION
 
